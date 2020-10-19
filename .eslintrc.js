c3748d14ecddb5aa11a1726e7467a0a315c02278
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-10-19 23:16:22 +0200

    chore: eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": ["eslint:recommended"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "indent": ["warn", 2],
-        "no-unused-vars": "warn",
-        "no-console": "warn",
-        "linebreak-style": "off",
-        "global-require": "warn"
-    }
+  "env": {
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": ["eslint:recommended"],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "rules": {
+    "indent": ["warn", 2],
+    "no-unused-vars": "warn",
+    "no-console": "warn",
+    "linebreak-style": "off",
+    "quotes": ["warn", "single"],
+    "global-require": "warn",
+    "semi": ["warn", "always"],
+    "no-console": "off"
+  }
 };
